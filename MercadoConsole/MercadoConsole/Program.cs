c454c5f891608f2b664904a312070042e27b8626
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-11-17 09:37:24 -0300

    17-11-23

---
+++ MercadoConsole/MercadoConsole/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace test
+{
+
+    class Produto //tudo referente aos produtos
+    {
+
+        public int r;
+
+        public void addProduto()
+        {
+            Console.WriteLine("Você escolheu para adicionar um produto!\n");
+            Console.Write("Digite o nome do produto que deseja adicionar:");
+        }
+
+        public void rmProduto()
+        {
+            Console.WriteLine("Você escolheu para remover um produto!");
+            Console.Write("Digite o id do produto que deseja remover:");
+
+        }
+
+    }
+
+    class Cliente : Produto //Tudo referente aos clientes 
+    {
+
+        List<int> notas = new List<int>();
+        int nota;
+
+        public void questionClient()
+        {
+            Console.WriteLine("Antes de sair, gostaria de colocar uma avaliação?");
+            Console.WriteLine("1 - Sim | Outro numero - Não");
+            r = Convert.ToInt32(Console.ReadLine());
+            if (r == 1)
+            {
+                addReview();
+            }
+            else
+            {
+                Console.WriteLine("Obrigado por ter usado nosso sistema!!!");
+                System.Environment.Exit(1);
+            }
+
+        }
+        public void addReview()
+        {
+            Console.Clear();
+            for (int s = 0; s <= 1; s++)
+            {
+                Console.Write("Digite a sua nota: ");
+                int nota = Convert.ToInt32(Console.ReadLine());
+                notas.Add(nota);
+            }
+            ViewReview();
+        }
+
+        void ViewReview()
+        {
+            Console.WriteLine("Notas inseridas:");
+            foreach (int nota in notas)
+            {
+                Console.WriteLine(nota);
+            }
+        }
+
+    }
+
+
+    class Program : Cliente
+        {
+
+
+            void Menu()
+            {
+                Console.WriteLine("Bem vindo ao sistema do supermecado\n");
+                Console.WriteLine("O que deseja realizar?\n");
+                Console.WriteLine("1 - Adicionar produto | 2 - Remover Produto | Qualquer outro numero - Sair\n");
+                r = Convert.ToInt32(Console.ReadLine());
+
+                switch (r)
+                {
+                    case 1:
+                        addProduto();
+                        break;
+
+                    case 2:
+                        rmProduto();
+                        break;
+
+                    default:
+                        //Console.WriteLine("Obrigado por ter usado o sistema!");
+                        questionClient();
+                        break;
+                }
+
+            }
+
+
+
+            static void Main(string[] args)
+            {
+                Program run = new Program();
+                run.Menu();
+            }
+        }
+
+
+    }
+
---
+++ test/test/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+
+namespace test
+{
+
+
+    class Program
+    {
+
+       
+
+        void GenerateNumbers()
+        {
+
+            Random random = new Random();
+
+
+            Console.WriteLine("Bem vindo ao gerador de números aleatorios");
+
+            Console.WriteLine("Quantos numeros você deseja obter?\n");
+            var quant = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o menor numero que você deseja:\n");
+            var min = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o maior numero que você deseja:\n");
+            var max = Convert.ToInt32(Console.ReadLine());
+
+            while (min > max)
+            {
+                Console.WriteLine("O valor minimo não pode ser maior que o valor maximo!\n");
+                Console.WriteLine("Digite outro valor minimo:\n");
+                min = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("O numeros gerados foram:");
+
+            List<int> numeros = new List<int>();
+            for (int runs = 0; runs <= quant; runs++)
+            {
+                var valor = random.Next(min, max);
+                numeros.Add(valor);
+
+            }
+
+            foreach (var valor in numeros)
+            {
+                Console.WriteLine(valor);
+            }
+
+            Console.WriteLine($"{quant} numeros foram gerados!");
+
+        }
+
+        static void Main(string[] args)
+        {
+            Program run = new Program();
+            run.GenerateNumbers();
+        }
+
+    }
+
+      
+    }
